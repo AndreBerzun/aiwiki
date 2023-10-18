6f83b103cd7201b12aa4c67efc14603d654791c4
Andre Berzun <andre.berzun@gmx.de>
2023-10-19 00:11:35 +0200

    Initial project setup

---
+++ aiwiki-cli/src/main/java/ch/lianto/aiwiki/AiWikiApplication.java
@@ -0,0 +1,13 @@
+package ch.lianto.aiwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiWikiApplication.class, args);
+    }
+
+}
