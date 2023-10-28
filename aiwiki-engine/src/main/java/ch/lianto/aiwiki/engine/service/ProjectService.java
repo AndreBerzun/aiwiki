985fe18fa9b038a1abbde9f08150d79cfddeb2c6
Andre Berzun <andre.berzun@gmx.de>
2023-10-28 13:06:32 +0200

    Implemented base methods of ProjectService

--- aiwiki-engine/src/main/java/ch/lianto/aiwiki/engine/service/ProjectService.java
+++ aiwiki-engine/src/main/java/ch/lianto/aiwiki/engine/service/ProjectService.java
@@ -1,19 +1,30 @@
 package ch.lianto.aiwiki.engine.service;
 
 import ch.lianto.aiwiki.engine.entity.Project;
+import ch.lianto.aiwiki.engine.repository.ProjectRepository;
 import ch.lianto.aiwiki.engine.service.data.ProjectDto;
+import ch.lianto.aiwiki.engine.service.data.mapper.ProjectMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class ProjectService {
-    public void createProject(ProjectDto dto) {
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
 
+    public ProjectService(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    public Project createProject(ProjectDto dto) {
+        Project project = projectMapper.fromDto(dto);
+        return projectRepository.save(project);
     }
 
     public List<Project> findAllProjects() {
-        return null;
+        return projectRepository.findAll();
     }
 
     public int importLocalPages(String projectName, String path) {
--- aiwiki-engine/src/test/java/ch/lianto/aiwiki/engine/testdata/TestDataProjects.java
+++ aiwiki-engine/src/test/java/ch/lianto/aiwiki/engine/testdata/TestDataProjects.java
@@ -1,17 +1,25 @@
 package ch.lianto.aiwiki.engine.testdata;
 
 import ch.lianto.aiwiki.engine.entity.Project;
+import ch.lianto.aiwiki.engine.service.data.ProjectDto;
 
 public class TestDataProjects {
     public final Project basic;
+    public final ProjectDto basicDto;
 
     public TestDataProjects() {
         basic = basicProject();
+        basicDto = basicDto();
     }
 
     public Project basicProject() {
         Project project = new Project();
         project.setName("Project");
+        return project;
+    }
+
+    private ProjectDto basicDto() {
+        ProjectDto project = new ProjectDto("Project");
         return project;
     }
 }
